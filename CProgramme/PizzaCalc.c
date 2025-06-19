dec7dbb0f3718b818206bb91b172693b4bbde201
Moritisimor <devMoritisimor@proton.me>
2025-06-19 13:35:48 +0200

    Made error message better to look at and fully translated to english

--- CProgramme/PizzaCalc.c
+++ CProgramme/PizzaCalc.c
@@ -1,13 +1,22 @@
 // PizzaRechner.py rewritten in C!
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define PI 3.14159
 #define SUCCESS 0
 #define WRONGTYPE 1
 
+void clearconsole() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
+
 int main() {
-    float radius1, radius2, preis1, preis2, preisproqcm1, preisproqcm2;
+    float radius1, radius2, price1, price2, pricepersqcm1, pricepersqcm2;
 
     printf("Welcome to the Pizza Calculator! \nThis tool will tell you, which of two pizzas costs less per square cm.");
 
@@ -20,20 +29,21 @@ int main() {
     int returnRadius2 = scanf(" %f", &radius2);
 
     printf("Enter the Price of the 1st pizza: ");
-    int returnPrice1 = scanf(" %f", &preis1);
+    int returnPrice1 = scanf(" %f", &price2);
 
     printf("Enter the price of the 2nd pizza: ");
-    int returnPrice2 = scanf(" %f", &preis2);
+    int returnPrice2 = scanf(" %f", &price2);
 
     if (returnPrice1 != 1 || returnPrice2 != 1 || returnRadius1 != 1 || returnRadius2 != 1) {
+        clearconsole();
         printf("\nAn Error has occurred!\nYou either typed a string, a character or used , instead of . for floating point integers.\nExiting...");
         return WRONGTYPE;
     }
 
-    preisproqcm1 = (PI * radius1 * radius1) / preis1;
-    preisproqcm1 = (PI * radius2 * radius2) / preis2;
+    pricepersqcm1 = (PI * radius1 * radius1) / price1;
+    pricepersqcm2 = (PI * radius2 * radius2) / price2;
 
-    if (preisproqcm1 > preisproqcm2) {
+    if (pricepersqcm1 > pricepersqcm2) {
         printf("The 1st pizza offers more value per square cm.");
     } else {
         printf("The 2nd pizza offers more value per square cm.");
