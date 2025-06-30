8d07fd2ea25d9f4edb1a8b0df5a660c0ec69c006
Moritisimor <devMoritisimor@proton.me>
2025-06-30 17:48:19 +0200

    Made StructsTest, a small program i will use to try programming with structs

---
+++ CPrograms/StructsTest.c
@@ -0,0 +1,18 @@
+struct Person {
+    char name[50];
+    char positon[50];
+    int yearsInService;
+};
+
+int main(void) {
+    struct Person People[] = { 
+        {"Gerald", "Mechanical Engineer", 8},
+        {"Mia", "Nurse", 4},
+        {"Jason", "Janitor", 5},
+        {"Katherine", "Digital Artist", 2},
+    };
+
+    for (int i = 0; i < 4; i++) {
+
+    }
+}
