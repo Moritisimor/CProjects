65a7b66d2a123555a931b782afe01bedfb23b1ed
Moritisimor <devMoritisimor@proton.me>
2025-07-02 21:04:39 +0200

    Slight optimization and error handling for negative numbers

--- CPrograms/Fibonacci.c
+++ CPrograms/Fibonacci.c
@@ -3,7 +3,7 @@
 #define INVALIDINPUT 1
 
 void fibonacci(int iterations) {
-  unsigned long long x = 1;
+  unsigned long long x = 0;
   unsigned long long y = 1;
 
   if (iterations >= 1) {
@@ -12,16 +12,13 @@ void fibonacci(int iterations) {
   if (iterations >= 2) {
     printf("iteration 2 = 1\n");
   }
-  if (iterations >= 3) {
-    printf("iteration 3 = 1\n");
-  }
   
-  for (int i = 4; i < iterations + 1 ; i++) {
+  for (int i = 3; i < iterations + 1 ; i++) {
     unsigned long long z = x + y;
     printf("iteration %d = %llu\n", i, z);
     x = y;
     y = z;
-    if (i == 91) {
+    if (i == 94) {
       printf("Cannot go above %d iterations due to bit limitations.\n", i);
       return;
     }
@@ -32,7 +29,7 @@ int main(void) {
   int iterations;
   printf("Welcome to my fibonacci program! \nEnter the amount of iterations you would like: ");
   int iterationsMatch = scanf("%d", &iterations);
-  if (iterationsMatch == 1) {
+  if (iterationsMatch == 1 && iterations > 0) {
     fibonacci(iterations);
     return SUCCESS;
   } else {
