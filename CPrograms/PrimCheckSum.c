c2e176a41709ecdd0d204c5022ab47ab534355d1
Moritisimor <devMoritisimor@proton.me>
2025-07-13 16:07:42 +0200

    Made PrimCheckSum more secure by adding subtraction if ASCII value is uneven and made sum unsigned

--- CPrograms/PrimCheckSum.c
+++ CPrograms/PrimCheckSum.c
@@ -1,34 +1,21 @@
 #include <stdio.h>
 
-void showPrimitiveCheckSum(char parsee[]) { 
+unsigned int getPrimitiveCheckSum(char parsee[]) {
   char *parseePtr = parsee;
-  int sum = 0;
+  unsigned int sum = 0;
   int spicer = 125;
-  while (1) {
-    if (*parseePtr != 0) {
-      printf("The character %c holds the ASCII value %d\n",*parseePtr, (int) *parseePtr);
-      sum += (int) *parseePtr * spicer;
-      spicer += 25;
-      parseePtr++;
+  while (*parseePtr != 0) {
+    sum += (int) *parseePtr * spicer;
+    if ((int) *parseePtr % 2 == 0) {
+      spicer += (int) *parseePtr;
     } else {
-      break;
+      if (spicer -= (int) *parseePtr > 0) {
+        spicer -= (int) *parseePtr; 
+      } else {
+        spicer += (int) *parseePtr;
+      }
     }
-  }
-  printf("The spiced-up checksum of these ASCII characters combined equals: %d\n", sum);
-}
-
-int getPrimitiveCheckSum(char parsee[]) {
-  char *parseePtr = parsee;
-  int sum = 0;
-  int spicer = 125;
-  while (1) {
-    if (*parseePtr != 0) {
-      sum += (int) *parseePtr * spicer;
-      spicer += 25;
-      parseePtr++;
-    } else {
-      break;
-    }
+    parseePtr++;
   }
   return sum;
 }
@@ -42,18 +29,28 @@ int comparePrimitiveCheckSums(char parsee1[], char parsee2[]) {
 }
 
 int main(void) {
-  char testString1[] = {'a', 'b', 'c', 'd', 'e', '\0'};
-  char testString2[] = {'e', 'd', 'c', 'b', 'a', '\0'};
-  showPrimitiveCheckSum(testString1);
-  showPrimitiveCheckSum(testString2);
-  printf("\nComparison of string abcde and edcba:\n");
+  char testString1[] = {'H', 'e', 'l', 'l', 'o', '\0'};
+  char testString2[] = {'W', 'o', 'r', 'l', 'd', '\0'};
+  printf("Checksum of Hello = %u\n", getPrimitiveCheckSum(testString1));
+  printf("Checksum of World = %u\n", getPrimitiveCheckSum(testString2));
+  
+  printf("\nComparison of string Hello and World:\n");
   if (comparePrimitiveCheckSums(testString1, testString2) == 1) {
     printf("Probably same.\n");
   } else {
     printf("Probably not Same.\n");
   }
-  printf("\nComparison string abcde with itself:\n");
+  
+  printf("\nComparison string Hello with itself:\n");
   if (comparePrimitiveCheckSums(testString1, testString1) == 1) {
+    printf("Probably same.\n");
+  } else {
+    printf("Probably not same.\n");
+  }
+
+  printf("\nComparison of string World with its checksum\n");
+  printf("Checksum = %u\n", getPrimitiveCheckSum(testString2));
+  if (25344 == getPrimitiveCheckSum(testString2)) {
     printf("Probably same.\n");
   } else {
     printf("Probably not same.\n");
