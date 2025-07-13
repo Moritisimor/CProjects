f5b3063ade4d24311808a3629a7cc8c581fbd9f6
Moritisimor <devMoritisimor@proton.me>
2025-07-13 12:22:12 +0200

    Added detection of null termination and made the example a bit clearer to showcase functionality in PrimCheckSum

--- CPrograms/PrimCheckSum.c
+++ CPrograms/PrimCheckSum.c
@@ -1,32 +1,40 @@
 #include <stdio.h>
 
-void showPrimitiveCheckSum(char parsee[], int length) { // Should not include null termination unless you want to print that 
+void showPrimitiveCheckSum(char parsee[]) { 
   char *parseePtr = parsee;
   int sum = 0;
   int spicer = 125;
-  for (int i = 0; i < length; i++) {
-    printf("The character %c holds the ASCII value %d\n",*parseePtr, (int) *parseePtr);
-    sum += (int) *parseePtr * spicer;
-    spicer += 25;
-    parseePtr++;
+  while (1) {
+    if (*parseePtr != 0) {
+      printf("The character %c holds the ASCII value %d\n",*parseePtr, (int) *parseePtr);
+      sum += (int) *parseePtr * spicer;
+      spicer += 25;
+      parseePtr++;
+    } else {
+      break;
+    }
   }
-  printf("The spiced-up checksum of these ASCII characters combined equals : %d\n", sum);
+  printf("The spiced-up checksum of these ASCII characters combined equals: %d\n", sum);
 }
 
-int getPrimitiveCheckSum(char parsee[], int length) {
+int getPrimitiveCheckSum(char parsee[]) {
   char *parseePtr = parsee;
   int sum = 0;
   int spicer = 125;
-  for (int i = 0; i < length; i++) {
-    sum += (int) *parseePtr * spicer;
-    spicer += 25;
-    parseePtr++;
+  while (1) {
+    if (*parseePtr != 0) {
+      sum += (int) *parseePtr * spicer;
+      spicer += 25;
+      parseePtr++;
+    } else {
+      break;
+    }
   }
   return sum;
 }
 
-int comparePrimitiveCheckSums(char parsee1[], char parsee2[], int length1, int length2) {
-  if (getPrimitiveCheckSum(parsee1, length1) == getPrimitiveCheckSum(parsee2, length2)) {
+int comparePrimitiveCheckSums(char parsee1[], char parsee2[]) {
+  if (getPrimitiveCheckSum(parsee1) == getPrimitiveCheckSum(parsee2)) {
     return 1;
   } else {
     return 0;
@@ -34,13 +42,20 @@ int comparePrimitiveCheckSums(char parsee1[], char parsee2[], int length1, int l
 }
 
 int main(void) {
-  char testString1[] = {'H', 'e', 'l', 'l', 'o', '\0'};
-  char testString2[] = {'W', 'o', 'r', 'l', 'd', '\0'};
-  showPrimitiveCheckSum(testString1, 5);
-  showPrimitiveCheckSum(testString2, 5);
-  if (comparePrimitiveCheckSums(testString1, testString2, 5, 5) == 1) {
+  char testString1[] = {'a', 'b', 'c', 'd', 'e', '\0'};
+  char testString2[] = {'e', 'd', 'c', 'b', 'a', '\0'};
+  showPrimitiveCheckSum(testString1);
+  showPrimitiveCheckSum(testString2);
+  printf("\nComparison of string abcde and edcba:\n");
+  if (comparePrimitiveCheckSums(testString1, testString2) == 1) {
     printf("Probably same.\n");
   } else {
     printf("Probably not Same.\n");
+  }
+  printf("\nComparison string abcde with itself:\n");
+  if (comparePrimitiveCheckSums(testString1, testString1) == 1) {
+    printf("Probably same.\n");
+  } else {
+    printf("Probably not same.\n");
   }
 }
